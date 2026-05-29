42150fcb9690afb9374018c353a14862a2697c2b
wangzhao <rxfmfqw@gmail.com>
2026-05-29 11:41:57 +0800

    chore: 添加 Apache 2.0 License 头和完善开源合规
    - 所有 Java 文件注入 Apache 2.0 License 头
    - 添加 Spotless Maven Plugin，validate 阶段自动检查
    - 新增 LICENSE、NOTICE 文件
    - 新增 style/license_header 模板

--- src/main/java/com/lanting/flink/sql/bootstrap/catalog/CatalogEntityFactory.java
+++ src/main/java/com/lanting/flink/sql/bootstrap/catalog/CatalogEntityFactory.java
@@ -1,7 +1,23 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package com.lanting.flink.sql.bootstrap.catalog;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.Schema;
 import org.apache.flink.table.catalog.*;
@@ -12,6 +28,9 @@ import org.apache.flink.table.types.AbstractDataType;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * 将 {@link CatalogEntity} 转换为 Flink 可直接使用的
--- src/main/java/com/lanting/flink/sql/bootstrap/executor/StreamingScriptExecutor.java
+++ src/main/java/com/lanting/flink/sql/bootstrap/executor/StreamingScriptExecutor.java
@@ -1,11 +1,27 @@
+/*
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 package com.lanting.flink.sql.bootstrap.executor;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lanting.flink.sql.bootstrap.resource.OperatorResourceSpec;
 import com.lanting.flink.sql.bootstrap.resource.OperatorSpec;
 import com.lanting.flink.sql.bootstrap.resource.ResourceEntity;
-import lombok.Getter;
-import lombok.Setter;
+
 import org.apache.flink.api.common.operators.SlotSharingGroup;
 import org.apache.flink.api.dag.Transformation;
 import org.apache.flink.configuration.CoreOptions;
@@ -28,11 +44,17 @@ import org.apache.flink.table.operations.Operation;
 import org.apache.flink.table.operations.StatementSetOperation;
 import org.apache.flink.util.StringUtils;
 
-import javax.annotation.Nullable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * 流模式下的 SQL Script 执行器，支持对 DML 算子做细粒度资源注入。
